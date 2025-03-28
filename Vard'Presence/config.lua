9d5aae53fa3fe91657ad2c0be4db6586131dfed8
Vardkor <153649323+Vardkor@users.noreply.github.com>
2025-03-28 15:10:03 +0100

    Update config.lua

--- Vard'Presence/config.lua
+++ Vard'Presence/config.lua
@@ -1,4 +1,4 @@
-config = {}
+Config = {}
 
 Config.DiscordAppID = 123456789012345678 -- Remplace par ton Application ID
 Config.BigImage = "big_logo" -- Nom de l'image principale
@@ -11,11 +11,9 @@ Config.Buttons = {
     {label = "Se Connecter", url = "fivem://connect/TON_IP"}
 }
 
-
 Config.SetRichPresenceText = "Survie dans un monde en ruine..." -- Texte sous le pseudo Discord
-Config.RefreshTime = 6000 --Temps de mise à jour en millisecondes (60s)
+Config.RefreshTime = 6000 -- Temps de mise à jour en millisecondes (60s)
 
-
-Config.ShowPlayerCount = true -- Afficher le nombre de joueurs connecter sur le serveur 
+Config.ShowPlayerCount = true -- Afficher le nombre de joueurs connectés sur le serveur
 Config.PlayerOnlineText = "👥 Il y a %s joueurs en ligne"
-Config.RefreshPlayerOnline = 6000 --Temps de mise à jour pour les joueurs en ligne en millisecondes pour (60s)
+Config.RefreshPlayerOnline = 6000 -- Temps de mise à jour pour les joueurs en ligne en millisecondes pour (60s)
